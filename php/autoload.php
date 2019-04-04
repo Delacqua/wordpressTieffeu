e07c7b12bf203501db82d45014d27d1a6e9debd6
Delacqua <delacqua@gmail.com>
2019-04-04 19:10:43 +0200

    aggiunto autoload e const

---
+++ php/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+//Var
+  const linkImage = "http://www.tieffeu.com/wordpress/wp-content/uploads/2019/03/noImg.png";
+  const linkImage404 = "http://www.tieffeu.com/wordpress/wp-content/uploads/2019/03/noImg2.jpg";
+  const hook1 = "[salta]";
+
+function autoload($class) {
+    $cartelle = array(
+          dirname(__FILE__).'/'
+    );
+
+    foreach($cartelle as $cartella) {
+      if (file_exists($cartella.$class . '.php')) {
+          require_once $cartella.$class . '.php'; 
+        } 
+    }
+}
+
+spl_autoload_register('autoload');
